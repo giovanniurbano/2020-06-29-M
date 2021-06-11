1492b97752a2b9b8e0f826d6222eda025bd83462
giovanniurbano <urbagio@gmail.com>
2021-06-11 14:14:15 +0200

    punto 1c

--- src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
+++ src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
@@ -6,11 +6,52 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import it.polito.tdp.imdb.model.Actor;
+import it.polito.tdp.imdb.model.Adiacenza;
 import it.polito.tdp.imdb.model.Director;
 import it.polito.tdp.imdb.model.Movie;
 
 public class ImdbDAO {
+	
+	public List<Adiacenza> getAdiacenze(Integer anno, Map<Integer, Director> idMap) {
+		String sql = "SELECT d1.id AS id1, d2.id AS id2, COUNT(*) AS peso "
+				+ "FROM directors d1, directors d2, movies m1, movies m2, movies_directors md1, movies_directors md2, roles r1, roles r2 "
+				+ "WHERE d1.id = md1.director_id "
+				+ "AND d2.id = md2.director_id "
+				+ "AND d1.id > d2.id "
+				+ "AND r1.actor_id = r2.actor_id "
+				+ "AND ( (md1.movie_id = m1.id AND md2.movie_id = m2.id AND md1.movie_id = md2.movie_id AND r1.movie_id = r2.movie_id AND r1.movie_id = m1.id AND r2.movie_id = m2.id) "
+				+ "OR (md1.movie_id <> md2.movie_id AND md1.movie_id = m1.id AND md2.movie_id = m2.id AND md1.movie_id = r1.movie_id AND md2.movie_id = r2.movie_id) ) "
+				+ "AND m1.year = m2.year AND m1.year = ? "
+				+ "GROUP BY d1.id, d2.id "
+				+ "HAVING peso > 0";
+		List<Adiacenza> result = new ArrayList<>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, anno);
+			
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				Director d1 = idMap.get(res.getInt("id1"));
+				Director d2 = idMap.get(res.getInt("id2"));
+				
+				if(d1 != null && d2 != null && !d1.equals(d2)) {
+					Adiacenza a = new Adiacenza(d1, d2, res.getDouble("peso"));
+					result.add(a);
+				}
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	
 	public List<Director> getDirectorsByYear(Integer year) {
 		String sql = "SELECT d.id, d.first_name, d.last_name "
--- src/main/java/it/polito/tdp/imdb/model/Model.java
+++ src/main/java/it/polito/tdp/imdb/model/Model.java
@@ -1,7 +1,9 @@
 package it.polito.tdp.imdb.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
@@ -13,6 +15,7 @@ import it.polito.tdp.imdb.db.ImdbDAO;
 public class Model {
 	private ImdbDAO dao;
 	private List<Director> vertici;
+	private Map<Integer, Director> idMap;
 	private Graph<Director, DefaultWeightedEdge> grafo;
 	
 	public Model() {
@@ -25,6 +28,15 @@ public class Model {
 		//vertici
 		this.vertici = new ArrayList<Director>(this.dao.getDirectorsByYear(anno));
 		Graphs.addAllVertices(this.grafo, this.vertici);
+		this.idMap = new HashMap<Integer, Director>();
+		for(Director d : this.vertici)
+			this.idMap.put(d.getId(), d);
+		
+		//archi
+		List<Adiacenza> archi = this.dao.getAdiacenze(anno, idMap);
+		for(Adiacenza arco : archi) {
+			Graphs.addEdge(this.grafo, arco.getD1(), arco.getD2(), arco.getPeso());
+		}
 		
 		return String.format("Grafo creato con %d vertici e %d archi\n", this.grafo.vertexSet().size(), this.grafo.edgeSet().size());
 	}
